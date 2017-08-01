1fd7e66cbe49bd94ea2114ef5ed5b764015bf83b
nda7 <nda7@pitt.edu>
2017-07-31 20:02:33 -0400

    added header comments

--- controller.ino
+++ controller.ino
@@ -1,29 +1,31 @@
-#include <Adafruit_Sensor.h>
+/******************************************************/
+// Filename: Controller.ino
+//
+// Description: This file is intended for use with the
+//              wearable step tracking device developed
+//              as a senior design project during the 
+//              Summer 2017 semester at the University
+//              of Pittsburgh. It is used to control
+//              the Arduino microcontrollers to enable
+//              proper calibration of the BNO055 IMU
+//              sensors and manage data collection.
+//
+// Authors:
+//    Nathan Anuskiewicz
+//    Michael Mortensen
+//    Jennifer Fang
+//
+//    Last Modified:     Modified By:          Comments:
+//    7/31/2017          Nathan Anuskiewicz    Initial Version
+//
+/*****************************************************/
 
+
+
+#include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include <SD.h>
 #include <Adafruit_BNO055.h>
-
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-   Connections
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 3-5V DC
-   Connect GROUND to common ground
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-   2015/AUG/27  - Added calibration and system status helpers
-*/
 
 /* Set the delay between fresh samples */
 #define BNO055_SAMPLERATE_DELAY_MS (10)
@@ -109,7 +111,7 @@ void setup(void)
   Serial.begin(9600);
   Serial.println("Orientation Sensor Test"); Serial.println("");
 
-  /* Initialise the sensor */
+  /* Initialize the sensor */
   if(!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
@@ -176,8 +178,7 @@ void setup(void)
 
 /**************************************************************************/
 /*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
+    Arduino loop function, called once 'setup' is complete
 */
 /**************************************************************************/
 void loop(void)
